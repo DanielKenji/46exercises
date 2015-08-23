40ceb3c12717ee093de058c70548398ed11621d6
Home\Daniel <dankenji27@gmail.com>
2015-08-23 20:12:36 -0300

    New concepts

---
+++ maxlist.py
@@ -0,0 +1,15 @@
+﻿# Function that takes a list of numbers and returns the largest.
+
+import random
+
+def max_in_list(numlist):
+    max = numlist[0]
+    for number in numlist:
+        if number > max:
+            max = number
+    return max
+            
+if __name__ == "__main__":
+    numlist = random.sample(range(1, 100), 6)
+    print(numlist)
+    print(max_in_list(numlist))
